3f4f71e8a571b87a7ea3089a865c1c9a4307a684
CoryG89 <CoryG89@gmail.com>
2013-05-05 00:45:30 -0500

    Removing unneeded and unused main.js file

--- javascripts/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('a').each(function (i) {
-        var href = this.attr('href');
-        if (href.search('.md') > 0)
-            this.attr('href', href.split('.md')[0]);
-    });
-});
