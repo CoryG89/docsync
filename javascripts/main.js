02ce597b80c4445d261bd1034183a6fd78ac7c18
CoryG89 <CoryG89@gmail.com>
2013-05-04 23:25:01 -0500

    Adding jquery script to update .md anchors to remove extension

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+$(function () {
+    $('a').each(function (i) {
+        var href = this.attr('href');
+        if (href.search('.md') > 0)
+            this.attr('href', href.split('.md')[0]);
+    });
+});
